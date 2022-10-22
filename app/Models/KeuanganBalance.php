7c188783c148a1c6f5a5c050f330510099ed24d5
Zaelani <bahar32harz@gmail.com>
2022-10-22 15:15:29 +0700

    first commit 1

---
+++ app/Models/KeuanganBalance.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KeuanganBalance extends Model
+{
+    use HasFactory;
+    protected $table = 'keuangan_balance';
+}
---
+++ app/Models/KeuanganPembayaranMahasiswa.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KeuanganPembayaranMahasiswa extends Model
+{
+    use HasFactory;
+    protected $table = 'keuangan_pembayaran_mahasiswa';
+}
